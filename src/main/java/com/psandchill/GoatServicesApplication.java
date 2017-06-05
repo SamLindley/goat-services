2284867bc17c4859223ddf0bc6b5c18167256668
Sam <samlindley0@gmail.com>
2017-06-06 00:15:32 +0200

    first commit

--- src/main/java/com/psandchill/GoatServicesApplication.java
+++ src/main/java/com/psandchill/GoatServicesApplication.java
@@ -26,9 +26,9 @@ public class GoatServicesApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... strings) throws Exception {
-		clearData();
+		/*clearData();
 		saveData();
-		showData();
+		showData();*/
 	}
 
 	private void showData() {
